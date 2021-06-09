7676b2ee6c4813a8a0859a54446b93c6746d6a01
DadAdmin <jsqcontracttohire@yahoo.com>
2021-06-08 19:39:53 -0500

    Fubared

---
+++ FlysBookStore-UI.WASM/Models/ResponceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlysBookStore_UI.WASM.Models
+{
+    public class ResponceModel
+    {
+        public bool Success { get; set; }
+
+        public string Error { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
---
+++ FlysBookStore-UI.WASM/Static/Endpoints.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlysBookStore_UI.WASM.Static
+{
+    public static class Endpoints
+    {
+        // 44311
+        //44357
+        // "applicationUrl": "http://localhost:63225",
+        // public static string BaseUrl = "http://localhost:63225";
+       // public static string BaseUrl = "http://localhost:44357";
+        public static string BaseUrl = "https://localhost:44357/";
+        public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
+        public static string BooksEndpoint = $"{BaseUrl}api/books/";
+        //Bad - Invalid URI: Invalid port specified.
+        //public static string RegisterEndpoint = $"{BaseUrl}api/users/register/";
+        //Good -needs the / before api
+        public static string RegisterEndpoint = $"{BaseUrl}api/users/register/";
+        public static string LogInEndpoint = $"{BaseUrl}api/users/login/";
+
+
+    }
+}
