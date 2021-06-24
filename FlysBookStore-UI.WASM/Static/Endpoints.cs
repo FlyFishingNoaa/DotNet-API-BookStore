9f1e130e817f0d7641c3ddff82882210fab46059
Scott Quillin <squillin@vergentlms.com>
2021-06-24 07:07:21 -0500

    Fixed EndPoints

--- FlysBookStore-UI.WASM/Static/Endpoints.cs
+++ FlysBookStore-UI.WASM/Static/Endpoints.cs
@@ -7,14 +7,13 @@ namespace FlysBookStore_UI.WASM.Static
 {
     public static class Endpoints
     {
-       // 44311
-       //44357
-       // "applicationUrl": "http://localhost:63225",
-       // public static string BaseUrl = "http://localhost:63225";
-       // public static string BaseUrl = "http://localhost:44357";
-       // public static string BaseUrl = "https://localhost:44357/";
-
-        public static string BaseUrl = "https://localhost:44358/";
+        // 44311
+        //44357
+        // "applicationUrl": "http://localhost:63225",
+        // public static string BaseUrl = "http://localhost:63225";
+        // public static string BaseUrl = "http://localhost:44357";
+        //public static string BaseUrl = "https://localhost:44358/";
+        public static string BaseUrl = "https://localhost:44357/";
         public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
         public static string BooksEndpoint = $"{BaseUrl}api/books/";
         //Bad - Invalid URI: Invalid port specified.
